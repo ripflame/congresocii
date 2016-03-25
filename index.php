9c891e5b9a6f2625017f0c53de2ecca34ea20ad1
Gilberto Leon <ripflame@gmail.com>
2016-03-25 10:18:47 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// echo "Hello World!";
+?>
+<html>
+  <head>
+    <title>Congreso de Ingenier&iacute;a</title>
+  </head>
+  <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+  <body>
+    <h1>Congreso de Ingenier&iacute;a</h1>
+  </body>
+</html>

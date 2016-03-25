3a06a3bb41c023faa89b5f5ce254984b9f618438
Gilberto Leon <ripflame@gmail.com>
2016-03-25 10:53:56 -0600

    Adding jquery

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
-<?php
-// echo "Hello World!";
-?>
+<!DOCTYPE html>
 <html>
   <head>
     <title>Congreso de Ingenier&iacute;a</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
   </head>
   <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
   <body>
-    <h1>Congreso de Ingenier&iacute;a</h1>
+    <div class="container">
+      <h1>My first bootstrap page</h1>
+      <p>This is some text</p>
+    </div>
   </body>
 </html>

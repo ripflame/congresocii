cbed5f41efa1e90afc00a7f62fe3fb50d4c65da4
Gilberto Leon <ripflame@gmail.com>
2016-04-03 11:21:41 -0500

    Adding other views

---
+++ registro.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Congreso de Ingenier&iacute;a e innovaci&oacute;n</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3 page-header">
+          <h1 class="text-center">Congreso de ingenier&iacute;a e innovaci&oacute;n</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+          <h3 class="text-center">Escribe el folio de tu boleto</h3>
+          <form action="#" method="post">
+            <div class="form-group">
+              <label for="folio">Folio</label>
+              <input value="" type="text" id="folio" name="folio" class="form-control">
+            </div>
+            <a href="index.php" class="btn btn-default">Regresar</a>
+            <button type="submit" class="btn btn-primary pull-right">Entrar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>

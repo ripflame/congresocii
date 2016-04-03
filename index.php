4a0513c4822476dead6171d83182cf466e3ed658
Gilberto Leon <ripflame@gmail.com>
2016-04-03 11:21:26 -0500

    Adding links

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Congreso de Ingenier&iacute;a</title>
+    <title>Congreso de Ingenier&iacute;a e innovaci&oacute;n</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
   </head>
-  <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
   <body>
     <div class="container">
-      <h1>My first bootstrap page</h1>
-      <p>This is some text</p>
+      <div class="page-header">
+        <h1 class="text-center">Congreso de ingenier&iacute;a e innovaci&oacute;n</h1>
+      </div>
+      <div class="row">
+        <div class="col-md-4 col-md-offset-4 btn-group-vertical">
+          <a href="registro.php" class="btn btn-primary">Registro</a>
+          <a href="seleccion.php" class="btn btn-primary">Selecci&oacute;n de actividades</a>
+          <a href=# class="btn btn-primary">P&aacute;gina del congreso</a>
+        </div>
+      </div>
     </div>
   </body>
 </html>

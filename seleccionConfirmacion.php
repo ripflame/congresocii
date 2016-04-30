42b799576c2935de2d268bfdba0174cff6e463cd
Gilberto Leon <ripflame@gmail.com>
2016-04-29 20:31:31 -0500

    Adding confirmation page

---
+++ seleccionConfirmacion.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Congreso de Ingenier&iacute;a e innovaci&oacute;n</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="bootstrap-validator/dist/validator.min.js" type="text/javascript"></script>
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+  </head>
+  <body>
+    <div class="container panel">
+      <div class="row">
+        <div class="col-sm-6 col-sm-offset-3 page-header">
+          <h1 class="text-center">Congreso de ingenier&iacute;a e innovaci&oacute;n</h1>
+          <h4 class="text-center">Selecci&oacute;n de actividades</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-4 col-sm-offset-4">
+          <h3 class="text-center">Hoja de registro</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+          <h4>Datos personales</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+          <div class="table-responsive">
+            <table cellspacing="0" class="table">
+              <thead>
+                <tr>
+                  <th>Folio</th>
+                  <th>Nombre</th>
+                  <th>Fecha de nacimiento</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>90</td>
+                  <td>Gilberto Andrés León Enríquez</td>
+                  <td>1990-12-25</td>
+                  <td>gilberto.leon990@gmail.com</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+          <h4>Escuela de procedencia</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+          <div class="table-responsive">
+            <table cellspacing="0" class="table">
+              <thead>
+                <tr>
+                  <th>Escuela</th>
+                  <th>Carrera</th>
+                  <th>Semestre</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Universidad Modelo</td>
+                  <td>Ingeniería Mecatrónica</td>
+                  <td>4</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+          <h4>Mi&eacute;rcoles - Concurso Pitch</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+          <div class="table-responsive">
+            <table cellspacing="0" class="table">
+              <thead>
+                <tr>
+                  <th>Título de pitch</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Fuck up nights!</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+          <h4>Jueves - Taller</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+          <div class="table-responsive">
+            <table cellspacing="0" class="table">
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Descripci&oacute;n</th>
+                  <th>Duraci&oacute;n</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Aquila</td>
+                  <td>Aprende cómo desarrollar tu propias aplicaciones de internet de las cosas, Inventa tus propios aparatos o adapta los ya existentes con la ayuda de Altair: la placa de desarrollo inalámbrica programable.</td>
+                  <td>3:00</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+          <h4>Viernes - Visita/Concurso</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+          <div class="table-responsive">
+            <table cellspacing="0" class="table">
+              <thead>
+                <tr>
+                  <th>Empresa</th>
+                  <th>Hora de inicio</th>
+                  <th>Duraci&oacute;n</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Dondé</td>
+                  <td>7:00</td>
+                  <td>6:00</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+          <a href="index.php" class="btn btn-default">Regresar</a>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
